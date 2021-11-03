b9b0ac2419ae57f7a2014a8fe0f17aa14285bd1d
Samuel Vicent <samuel.vicent@takeachef.com>
2021-11-03 20:36:13 +0100

    Merge From Master

---
+++ src/Context/Admin/Post/Resolver/MesesResolver.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\Context\Admin\Post\Resolver;
+
+final class MesesResolver
+{
+    public static function resolve(): array{
+        $meses = [];
+        for ($i=1;$i<=12;$i++) {
+            $nombreDeMes = \DateTime::createFromFormat('!m', (string)$i)->format('F');
+            $meses[$nombreDeMes] = $i;
+        }
+        return $meses;
+    }
+}
