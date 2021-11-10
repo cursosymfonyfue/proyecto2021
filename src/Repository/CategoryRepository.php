e2389a9976dcecd6d71494a040323da1580eb31e
Samuel Vicent <samuel.vicent@takeachef.com>
2021-11-10 20:43:34 +0100

    remove dummy namespace

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Repository;
 
-use App\Core\Entity\Images;
 use App\Entity\Category;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
