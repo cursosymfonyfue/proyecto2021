9c94080400ef8ddd358cd1a37c78800bc01f1d42
Samuel Vicent <samuel.vicent@takeachef.com>
2021-11-26 18:14:14 +0100

    configure xdebug

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+set_time_limit(300);
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
